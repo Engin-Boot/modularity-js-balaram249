3681492d391cfb0984d30856448b792f3646255f
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-08-25 13:42:07 +0530

    testing if evaluate is loaded or not

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 const evaluate = require('./evaluate');
-const tests = require('./tests');
-console.log("color coder");
-testNumberToPair(4, "white", "brown");
-testNumberToPair(25, "violet", "slate");
-testColorToNumber("black", "brown", 14);
-testColorToNumber("yellow", "green", 18);
+console.log(GetColorFromPairNumber(25));

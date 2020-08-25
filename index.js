e23f4bae7a61755d28e2ea824395868eec289721
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-08-25 13:50:22 +0530

    tests added

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 var evaluate = require('./evaluate');
-console.log(evaluate.GetColorFromPairNumber(25));
+var tests=require('./evaluate');
+console.log("color coder");
+tests.testNumberToPair(4, "white", "brown");
+tests.testNumberToPair(25, "violet", "slate");
+tests.testColorToNumber("black", "brown", 14);
+tests.testColorToNumber("yellow", "green", 18);

a874e6966c10a01d7d926356274aa0b3675a5cb2
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-08-25 13:01:06 +0530

    print statement added for GetPairNumberFromColors

--- index.js
+++ index.js
@@ -39,3 +39,4 @@ function GetPairNumberFromColors(majorColor, minorColor) {
     }
     return majorIndex * MinorColors.length + minorIndex + 1;
 }
+console.log(GetPairNumberFromColors(violet,slate));

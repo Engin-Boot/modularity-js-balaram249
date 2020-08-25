2d4316c54f2699ec3408f8c4be09982be4c68054
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-08-25 13:16:14 +0530

    tests seperated

---
+++ tests.js
@@ -0,0 +1,20 @@
+const expect = require('chai').expect;
+const index = require('./index');
+function testNumberToPair(number, expectedMajor, expectedMinor) {
+    const pairOfColors = GetColorFromPairNumber(number);
+    console.log(`${number} = ${expectedMajor} ${expectedMinor}`);
+    expect(pairOfColors.major).equals(expectedMajor);
+    expect(pairOfColors.minor).equals(expectedMinor);
+}
+
+function testColorToNumber(majorColor, minorColor, expectedNumber) {
+    const pairNumber = GetPairNumberFromColors(majorColor, minorColor);
+    console.log(`${majorColor} ${minorColor} = ${pairNumber}`);
+    expect(pairNumber).to.equal(expectedNumber);
+}
+
+console.log("color coder");
+testNumberToPair(4, "white", "brown");
+testNumberToPair(25, "violet", "slate");
+testColorToNumber("black", "brown", 14);
+testColorToNumber("yellow", "green", 18);

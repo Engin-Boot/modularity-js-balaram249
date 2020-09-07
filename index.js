3e9f82e0d3976371d6181254a6a35eb8127711fd
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-09-08 01:51:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-//var evaluate = require('./evaluate');
-var tests=require('./tests');
-console.log("color coder");
-tests.testNumberToPair(4, "white", "brown");
-tests.testNumberToPair(25, "violet", "slate");
-tests.testColorToNumber("black", "brown", 14);
-tests.testColorToNumber("yellow", "green", 18);
+var evaluate = require('./evaluate');
+console.log(evaluate.GetPairNumberFromColors("violet","slate"));
+//var tests=require('./tests');
+//console.log("color coder");
+//tests.testNumberToPair(4, "white", "brown");
+//tests.testNumberToPair(25, "violet", "slate");
+//tests.testColorToNumber("black", "brown", 14);
+//tests.testColorToNumber("yellow", "green", 18);

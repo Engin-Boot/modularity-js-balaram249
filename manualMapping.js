ec1fb5898c5e6d138aee8cd20ff7b5e64905ac68
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-09-08 05:53:30 +0530

    returns the manual

---
+++ manualMapping.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const evaluate = require('./evaluate');
+//const expect = require('chai').expect;
+const tests = require('./tests');
+const { GetColorFromPairNumber } = require('./evaluate');
+function toString(){
+    var manualMapping =new Map();
+    let temporaryPairNumber;
+    for(temporaryPairNumber=1;temporaryPairNumber<26;temporaryPairNumber++){
+        let temporaryColorPair=GetColorFromPairNumber(temporaryPairNumber);
+        manualMapping.set(temporaryPairNumber,temporaryColorPair);
+
+    }
+ //   console.log(manualMapping);
+    return manualMapping;
+}
+var temp=toString();
+console.log(temp);

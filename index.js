124ef9fd2d22378a00019f466720849f1a1a8b8c
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-09-08 05:54:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var evaluate = require('./evaluate');
-console.log(evaluate.GetPairNumberFromColors("violet","slate"));
 var tests=require('./tests');
-//console.log("color coder");
-tests.testNumberToPair(4, "white", "brown");
-tests.testNumberToPair(25, "violet", "slate");
-tests.testColorToNumber("black", "brown", 14);
-tests.testColorToNumber("yellow", "green", 18);
+var manualMapping=require('./manualMapping.js');
+console.log(manualMapping.toString());
